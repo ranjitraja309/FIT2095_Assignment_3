f1cd2d66fde2d2119617442cf7e70f92e44f6e24
ranjitraja309 <rbal0013@student.monash.edu>
2024-08-23 18:59:22 +1000

    Completed delete driver, and added page not found html and endpoint

--- app.js
+++ app.js
@@ -21,11 +21,6 @@ app.listen(PORT, () => {
     console.log(`running http://localhost:${PORT}`);
 });
 
-// Page not found Endpoint
-app.get("*", function (request, response) {
-	response.render('pagenoutfound.html');
-});
-
 // Home Page Endpoint
 app.get('/', (req, res) => {
     res.render('index.html');
@@ -53,21 +48,14 @@ app.get('/30628059/Ranjit/drivers/delete', (req, res) => {
     res.render('deletedriver.html', { records: driversDB });
 });
 
-// For incorrect Driver ID deletion
-app.get('/30628059/Ranjit/drivers/delete/IDcheck', (req, res) => {
-    const driverId = req.query.driver_id;
-    
-    if (driversDB.findIndex(driver => driver.driver_id === driverId) === -1) {// if there is a mismatch in the ID - will redirect to invalid data page
-        res.render('invaliddata.html');
-    } else {
-        driversDB.splice(driverIndex, 1);  //if ID is correct, it will remove from driversDB
-        res.redirect('/30628059/Ranjit/drivers');
-    }
-});
-
 // Delete Driver POST req
 app.post('/30628059/Ranjit/drivers/delete', (req, res) => {
     const driverId = req.body.driver_id;
     driversDB = driversDB.filter(driver => driver.driver_id !== driverId);  
     res.redirect('/30628059/Ranjit/drivers');
+});
+
+// Page not found Endpoint
+app.get('*', (req, res) => {
+	res.render('pagenoutfound.html');
 });
