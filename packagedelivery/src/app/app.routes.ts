5ffe26c08402bbb20ee37a05e31008266a0ed653
ranjitraja309 <rbal0013@student.monash.edu>
2024-10-09 13:44:58 +1100

    added page not found component

--- packagedelivery/src/app/app.routes.ts
+++ packagedelivery/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Other routes will go here
+  { path: '**', component: PagenotfoundComponent }
+];
---
+++ packagedelivery/src/app/pagenotfound/pagenotfound.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagenotfound',
+  standalone: true,
+  template: '<h1>Page Not Found!</h1>',
+  styles: ['h1 { text-align: center }']
+})
+export class PagenotfoundComponent { }
